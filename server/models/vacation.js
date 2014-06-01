2a02853e001f6b1f18a937071fc514abbf5d6635
Paul Garcia <pbrain19@gmail.com>
2014-06-01 13:14:35 -0400

    new data structure

--- server/models/vacation.js
+++ server/models/vacation.js
@@ -14,7 +14,16 @@ var vacation = mongoose.Schema({
     q8:String,
     q9:String,
     q10:String,
-    q11:String
+    q11:String ,
+    q12:String,
+    q13:String,
+    q14:String,
+    q15:String,
+    q16: String,
+    q17:String,
+    q18:String,
+    q19:String,
+    q20:String
     
 });
 
